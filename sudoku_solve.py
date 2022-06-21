81b4549f368ef812250d66580ba31016d5c2d869
qwert42random <Cskywalker1@gmail.com>
2022-06-21 18:20:30 +0100

    Added function to obtain grids
    Function to obtain grids will return matrix with all number in same grid
    grouped together.
    Added a node class.

--- sudoku_solve.py
+++ sudoku_solve.py
@@ -5,30 +5,54 @@ outputFile = open("output.txt", "w")
 def parseInput(fileName):
 
     inputFile = open(fileName, "r")
-    matrix = []
-    matrix_line = []
+    matrix = np.zeros([9,9], dtype=int)
 
     # Iterate through all lines in input file.
-    for line in inputFile.read().split("\n"):
+    for row, line in enumerate(inputFile.read().split("\n")):
+        for column, input in enumerate(line.split(",")):
 
-        for char in line.split(","):
-
-            # If space, then None. Otherwise, is number.
-            if char == " ":
-                matrix_line.append(None)
+            # Fill matrix with input.
+            if input == " ":
+                matrix[row, column] = 0 
             else:
-                matrix_line.append(int(char))
+                matrix[row, column] = int(input)
 
-        matrix.append(matrix_line)
-        matrix_line = []
+    return matrix
 
-    return np.matrix(matrix)
+def obtainGrids(matrix):
+    grids = []
+
+    # Print first row of grids of puzzle.
+    for i in range(0, 9, 3):
+        grids.append([inputMatrix[i:i+3, 0:3], inputMatrix[i:i+3, 3:6], inputMatrix[i:i+3, 6:9]])
+    
+    return grids
 
 inputMatrix = parseInput("testInput.txt")
-print(inputMatrix)
+sudokuGrids = obtainGrids(inputMatrix)
 print()
+print(sudokuGrids)
 
-# Print first row of grids of puzzle.
+exit()
+
+# Print second row of grids of puzzle.
 for i in [0, 3, 6]:
-    print(inputMatrix[i:i+3, 0:3])
+    print(inputMatrix[3:6, i:i+3])
     print()
+
+# Print third row of grids of puzzle.
+for i in [0, 3, 6]:
+    print(inputMatrix[6:9, i:i+3])
+    print()
+
+possibleNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+class node:
+    def __init__(self, matrix):
+        self.visited = False
+        self.possibleNumbers = []
+        self.matrix = matrix 
+        self.x = None
+        self.y = None
+        self.children = []
+        pass
